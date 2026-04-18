3b855a1bfc9ca4727465027466e696bec18dc7ba
Lőrik Levente <levminer@levminer.com>
2026-04-18 16:29:20 +0200

    főoldal

--- templates/pages/cimlap.tpl.php
+++ templates/pages/cimlap.tpl.php
@@ -2,12 +2,20 @@
     Mitől különleges a pizzériánk?
 </h2>
 
-<img src="./images/pizza.jpg" alt="Árpádhalom" class="img-responsive">
+<img src="./images/pizza.jpg" alt="Árpádhalom" class="img-responsive cimlap-kep">
 
 <p>
-    A Nonstop SMS Pizzéria éjjel-nappal várja a pizzarajongókat. Egyszerű, gyors és kényelmes csak küldj egy üzenetet! Minden SMS egyetlen pizzára vonatkozik, így a rendelés mindig átlátható.
+    A Nonstop SMS Pizzéria éjjel-nappal várja a pizzarajongókat. Egyszerű, gyors és kényelmes a rendelés: csak küldj egy üzenetet. Minden SMS egyetlen pizzára vonatkozik, így a rendelés mindig átlátható.
+</p>
 
-Folyamatos nyitvatartásunknak köszönhetően bármikor csillapíthatod az éhséget. Akár egy hosszú műszak után, akár egy buli közben, ránk számíthatsz. Kínálatunkban klasszikus és különleges pizzák is megtalálhatók.
+<p>
+    Folyamatos nyitvatartásunknak köszönhetően bármikor csillapíthatod az éhséget. Akár egy hosszú műszak után, akár egy buli közben, ránk számíthatsz. Kínálatunkban klasszikus és különleges pizzák is megtalálhatók.
+</p>
 
-A rendelésedet perceken belül visszaigazoljuk SMS-ben. A Nonstop SMS Pizzéria ahol a nap 24 órájában csak egy üzenet választ el a kedvenc pizzádtól.
+<p>
+    A rendelésedet perceken belül visszaigazoljuk SMS-ben, hogy pontosan tudd, mikor indul hozzád a kedvenc pizza. Célunk, hogy gyors, megbízható és finom megoldást adjunk minden napszakban.
+</p>
+
+<p>
+    A Nonstop SMS Pizzéria, ahol a nap 24 órájában csak egy üzenet választ el a frissen sült pizzádtól.
 </p>
